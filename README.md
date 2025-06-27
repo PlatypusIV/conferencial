37f5d69070b972208dd7aed111bb3dcac5bbc68b
Herbert-Ken Ymera <hkymera2@gmail.com>
2025-06-27 20:21:29 +0300

    fixed dockerfiles and updated readme

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 Hello.
 
-This is my test assignment.
+This was a test assignment.
 
 In order to use it simply follow these steps
 
 1: clone the repo
 2: cd into file
-3: docker compose up
+3: docker compose up --build -d
+3b: run all services
 4: open browser on localhost:8080
 
-Tried to make app as simple as possible and while I lack any design abilities I will improve it over time.
+Tried to make app as simple as possible.
+My design abilites still require some work.
--- conferencial-frontend/src/store/store.ts
+++ conferencial-frontend/src/store/store.ts
@@ -1,21 +1,18 @@
-import {configureStore, createListenerMiddleware} from '@reduxjs/toolkit';
-import conferenceReducer from './conferenceActions';
-import participantReducer from './participantActions';
-import userInterfaceReducer from './userInterfaceActions';
-import roomReducer from './roomActions';
-
-const listenerMiddleware = createListenerMiddleware();
+import { configureStore } from "@reduxjs/toolkit";
+import conferenceReducer from "./conferenceActions";
+import participantReducer from "./participantActions";
+import userInterfaceReducer from "./userInterfaceActions";
+import roomReducer from "./roomActions";
 
 const store = configureStore({
-    reducer:{
-        conference: conferenceReducer,
-        participant: participantReducer,
-        userInterface: userInterfaceReducer,
-        room: roomReducer,
-    },
-    // middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().prepend(listenerMiddleware.middleware)
+  reducer: {
+    conference: conferenceReducer,
+    participant: participantReducer,
+    userInterface: userInterfaceReducer,
+    room: roomReducer,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
